7ec2e02c90106ff26eb50fe892a99ae6f0205c17
Ash Nandan <ashr5nandan@gmail.com>
2026-08-27 23:59:01 +1200

    create my personal project: MoneyTrail

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,16 +1,7 @@
-import { useFruits } from '../hooks/useFruits.ts'
+import Home from "./Home"
 
-function App() {
-  const { data } = useFruits()
-
-  return (
-    <>
-      <div className="app">
-        <h1>Fullstack Boilerplate - with Fruits!</h1>
-        <ul>{data && data.map((fruit) => <li key={fruit}>{fruit}</li>)}</ul>
-      </div>
-    </>
-  )
+function App () {
+  return <Home />
 }
 
 export default App
---
+++ client/components/Home.tsx
@@ -0,0 +1,37 @@
+function Home () {
+  return (
+    <main>
+      <section>
+        <h1>MoneyTrail</h1>
+        <p>Follow Where Your Money Goes!</p>
+      </section>
+
+      <section>
+        <h2>About MoneyTrail</h2>
+        <p>MoneyTrail is a personal finance application I'm building to better understand where my money goes and how my spending changes over time.</p>
+      </section>
+
+      <section>
+        <h2>Why I'm Building It?</h2>
+        <p>MoneyTrail started with a personal finance spreadsheet. I used Google Sheets to track where I spent my money, how much I've spent, what remained in my main account,
+          and how much money I had accross my spending and savings accounts.
+        </p>
+
+        <p>After spending so much time tracking my finances manually, I wanted to turn that system into something of my own. Watching Caleb Hammer's Financial Audit and hearing about DollarWise 
+          also inspired me to explore what personal finance application could look like.
+        </p>
+
+        <p>MoneyTrail brings together two things I'm genuinely interested in: personal finances and software development. Instead of relying on my spreadsheet, I want to gradually build the tools 
+          I use to understand my finances into my own fullstack web application.
+        </p>
+      </section>
+
+      <section>
+        <h2>Built With:</h2>
+        <p>React | TypeScript | Node.js | SQL | Knex.js</p>
+      </section>
+    </main>
+  )
+}
+
+export default Home
